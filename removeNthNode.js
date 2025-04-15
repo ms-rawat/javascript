0f91c97b8063fecc1086f61adb5ff08320100da9
moharwisess <mohar.rawat@wisess.in>
2025-04-15 09:27:34 +0530

    Add function to remove the N-th node from the end of a linked list

---
+++ removeNthNode.js
@@ -0,0 +1,20 @@
+function removeNthNode(head,n){
+    const dummy = new ListNode(0,head);
+    let fast = dummy;
+    let slow = dummy;
+
+    for(let i=0;i<=n;i++)
+    {
+        fast = fast.next;
+    }
+
+
+while(fast!=null)
+{
+    fast = fast.next;
+    slow = slow.next;
+}
+slow.next = slow.next.next;
+
+return dummy.next;
+}
